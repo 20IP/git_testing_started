8dccb4e23b111a4da61fcb5d4515b37fea470632
20IP <phamthai.ats@gmail.com>
2022-04-15 22:29:26 +0700

    Add new 3 model defiend

--- new_branch_test.py
+++ new_branch_test.py
@@ -1,3 +1,12 @@
 import os
 
 print('New branch')
+
+def model_01():
+    print('Processing model')
+    
+def model_02():
+    print('Processing model_02')
+    
+def model_03():
+    print('Processing model_03')
